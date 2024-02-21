4cc76bdfefe4b723410443d13104155d0a6786e5
lashagelashvili <las.gelashvili@gmail.com>
2024-02-22 00:08:59 +0400

    add logout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,21 +4,37 @@ import { MessageService } from '../shared/data-access/message.service';
 import { MessageInputComponent } from './ui/message-input.component';
 import { AuthService } from '../shared/data-access/auth.service';
 import { Router } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   standalone: true,
   selector: 'app-home',
   template: `
     <div class="container">
+      <mat-toolbar color="primary">
+        <span class="spacer"></span>
+        <button mat-icon-button (click)="authService.logout()">
+          <mat-icon>logout</mat-icon>
+        </button>
+      </mat-toolbar>
+
       <app-message-list [messages]="messageService.messages()" />
       <app-message-input (send)="messageService.add$.next($event)" />
     </div>
   `,
-  imports: [MessageListComponent, MessageInputComponent],
+  imports: [
+    MessageListComponent,
+    MessageInputComponent,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+  ],
 })
 export default class HomeComponent {
   messageService = inject(MessageService);
-  private authService = inject(AuthService);
+  authService = inject(AuthService);
   private router = inject(Router);
 
   constructor() {
