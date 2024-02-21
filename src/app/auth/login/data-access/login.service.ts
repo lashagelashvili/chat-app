f6629569ef2c943488ee843e00c287eaed6a5b35
lashagelashvili <las.gelashvili@gmail.com>
2024-02-21 23:51:24 +0400

    add auth services

---
+++ src/app/auth/login/data-access/login.service.ts
@@ -0,0 +1,51 @@
+import { Injectable, computed, inject, signal } from '@angular/core';
+import { EMPTY, Subject, merge, switchMap } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { connect } from 'ngxtension/connect';
+import { AuthService } from '../../../shared/data-access/auth.service';
+import { Credentials } from '../../../shared/interfaces/credentials';
+
+export type LoginStatus = 'pending' | 'authenticating' | 'success' | 'error';
+
+interface LoginState {
+  status: LoginStatus;
+}
+
+@Injectable()
+export class LoginService {
+  private authService = inject(AuthService);
+
+  // sources
+  error$ = new Subject<any>();
+  login$ = new Subject<Credentials>();
+
+  userAuthenticated$ = this.login$.pipe(
+    switchMap((credentials) =>
+      this.authService.login(credentials).pipe(
+        catchError((err) => {
+          this.error$.next(err);
+          return EMPTY;
+        })
+      )
+    )
+  );
+
+  // state
+  private state = signal<LoginState>({
+    status: 'pending',
+  });
+
+  // selectors
+  status = computed(() => this.state().status);
+
+  constructor() {
+    // reducers
+    const nextState$ = merge(
+      this.userAuthenticated$.pipe(map(() => ({ status: 'success' as const }))),
+      this.login$.pipe(map(() => ({ status: 'authenticating' as const }))),
+      this.error$.pipe(map(() => ({ status: 'error' as const })))
+    );
+
+    connect(this.state).with(nextState$);
+  }
+}
---
+++ src/app/auth/register/data-access/register.service.ts
@@ -0,0 +1,50 @@
+import { Injectable, computed, inject, signal } from '@angular/core';
+import { connect } from 'ngxtension/connect';
+import { EMPTY, Subject, catchError, map, merge, switchMap } from 'rxjs';
+import { AuthService } from '../../../shared/data-access/auth.service';
+import { Credentials } from '../../../shared/interfaces/credentials';
+
+export type RegisterStatus = 'pending' | 'creating' | 'success' | 'error';
+
+interface RegisterState {
+  status: RegisterStatus;
+}
+
+@Injectable()
+export class RegisterService {
+  private authService = inject(AuthService);
+
+  // sources
+  error$ = new Subject<any>();
+  createUser$ = new Subject<Credentials>();
+
+  userCreated$ = this.createUser$.pipe(
+    switchMap((credentials) =>
+      this.authService.createAccount(credentials).pipe(
+        catchError((err) => {
+          this.error$.next(err);
+          return EMPTY;
+        })
+      )
+    )
+  );
+
+  // state
+  private state = signal<RegisterState>({
+    status: 'pending',
+  });
+
+  // selectors
+  status = computed(() => this.state().status);
+
+  constructor() {
+    // reducers
+    const nextState$ = merge(
+      this.userCreated$.pipe(map(() => ({ status: 'success' as const }))),
+      this.createUser$.pipe(map(() => ({ status: 'creating' as const }))),
+      this.error$.pipe(map(() => ({ status: 'error' as const })))
+    );
+
+    connect(this.state).with(nextState$);
+  }
+}
